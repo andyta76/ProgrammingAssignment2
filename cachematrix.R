556cc4a8df882712aa90e9ed8d88ff79b0f1a201
Andy Ta <ta0881g@MOHHNB0769.healthgrp.com.sg>
2014-09-21 00:01:25 +0800

    R Programming Assignment Week 2 submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix is a function which allows a matrix to be cache
+## Contains sub-methods: get(), set(matrix), setsolve(matrix)
+## and getsolve()
 
-makeCacheMatrix <- function(x = matrix()) {
+## written by: Andy Ta
+## Date: 18/09/2014 for R Programming Assignment 2
 
+
+makeCacheMatrix <- function(x = numeric()) {
+        i <- NULL
+        set <- function(y) {
+                x <<- y
+                i <- NULL
+        }
+        get <- function() {
+                x
+        }
+        setsolve <- function(inverse) {
+                i <<- inverse
+        }
+        getsolve <- function() {
+                i
+        }
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve is a function which caches the inverse of a matrix input
+## Uses makeCacheMatrix function
+
+## written by: Andy Ta
+## Date: 18/09/2014 for R Programming Assignment 2
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getsolve()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setsolve(m)
+        m
 }
